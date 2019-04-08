ffb128c3a147f01a4b08a83014645db6a498f353
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-04-08 23:16:32 +0700

    Update serverless

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -2,11 +2,6 @@ const mysql = require("mysql");
 
 let { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
-DB_HOST = "laravelins.cyamlpynsgan.ap-northeast-1.rds.amazonaws.com";
-DB_USERNAME = "root";
-DB_PASSWORD = "root1234";
-DB_NAME = "laraveldb";
-
 const pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USERNAME,
